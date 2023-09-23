ab939c8499fe3acaf6dc220015004d60b7349ed1
arpitachauhan41 <137207545+arpitachauhan41@users.noreply.github.com>
2023-09-23 20:17:58 +0530

    Add files via upload

---
+++ test.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(){
+    int age=22;
+    char star='*';
+    float pi=3.14;
+    printf("age is %d/n",age);
+    printf("star is %c/n",star);
+    printf("float is %f/n",pi);
+    return 0;
+}
